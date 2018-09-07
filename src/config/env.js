f237f5d6b6776ed979fb1f951b058c701dd06339
Mehmet Mutlu Elmas <mehmetmutluelmas@gmail.com>
2018-09-07 13:07:19 +0300

    config/env api url changed

--- src/config/env.js
+++ src/config/env.js
@@ -1,2 +1,1 @@
-//export const API_BASE = 'http://192.81.212.147:3001/api' 
-export const API_BASE = 'http://127.0.0.1:3001/api' 
+export const API_BASE = 'https://nodejs-express-mysql-crud.herokuapp.com/api' 
